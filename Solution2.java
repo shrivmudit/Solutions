eda449dc827142e942d71b5a33dbeb732336243a
shrivmudit <46938418+shrivmudit@users.noreply.github.com>
2021-08-10 23:38:25 +0530

    Add files via upload

---
+++ Solution2.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.*;
+public class Solution2 {
+
+    public static void main(String[] args) {
+
+                int n1,sum=0, sum1=0;
+
+                Scanner s=new Scanner(System.in);
+
+
+                int n= s.nextInt();
+
+                while(n>0) {
+                    n1 = n % 10;
+                    if (n1 % 2 == 0) {
+                        sum = sum + n1;
+                    }
+                    n = n / 10;
+                }
+                if(sum==sum1) {
+                    System.out.println("Yes");
+                }
+                else
+                    System.out.println("No");
+
+            }
+
+        }
